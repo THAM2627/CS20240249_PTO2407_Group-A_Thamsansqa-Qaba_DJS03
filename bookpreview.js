239de7dd11c8ffbe6ce6aa56a0cfc8281f6a34fa
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-05-18 22:29:37 +0200

    nook preview build

---
+++ bookpreview.js
@@ -0,0 +1,18 @@
+class BookPreview extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({ mode: "open" });
+        this.render();
+    }
+}
+
+render() {
+    const template = document.createElement("template");
+    template.innerHTML = `
+        <div>
+            <img src="${this.image}" />
+            <h2>${this.title}</h2>
+            <p>${this.author}</p>            
+        </div>`
+        this.ShadowRoot.innerHTML = template
+}
