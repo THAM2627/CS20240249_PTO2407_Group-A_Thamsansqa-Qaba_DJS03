3a41c0ecbef4060e63ddf9ddd44eb30e3148123d
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-05-18 13:38:59 +0200

    Loading data from JSON files

--- scripts.js
+++ scripts.js
@@ -52,6 +52,12 @@ function applyfilers (books, filters) {
 
 const app = document.getElementById('app');
 
+const booksData = [
+    { title: 'Book 1', author: 'Author 1', genre:'Genre 1'},
+    { title: 'Book 2', author: 'Author 2', genre:'Genre 2'},
+    { title: 'Book 3', author: 'Author 3', genre:'Genre 3'}
+];
+
 
 
 
